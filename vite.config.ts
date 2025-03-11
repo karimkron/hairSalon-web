354796deaad6f6352cc594ea673e233452f50685
sarayus300 <sarayusperlata@gmail.com>
2025-03-11 02:44:33 +0100

    00.00.04

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['hairsalon-web.onrender.com', 'localhost', '127.0.0.1']
+  }
 })
